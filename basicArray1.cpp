9d53aa6b605482a8bf258707e932c95dfa17276a
Jagriti <ruchigiri88@gmail.com>
2026-07-21 01:09:15 +0530

    Added a basic program of array

---
+++ basicArray1.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+
+    int marks[5] = { 50, 45, 67, 82, 84};
+
+    cout<< marks[0]<< endl; // 0th position // 50
+    cout<< marks[3] << endl; // 3rd position // 67
+
+    return 0;
+}
